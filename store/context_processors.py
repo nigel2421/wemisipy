241da0e0fb0f4657b54765a2642757a2754fd090
nigel2421 <nigel2421@yahoo.com>
2026-01-23 12:51:30 +0300

    wewe website wewe

--- store/context_processors.py
+++ store/context_processors.py
@@ -1,13 +1,42 @@
-from .models import Product, Category
+from .models import Category
+
 
 def categories_processor(request):
-    return {'menu_categories': Category.objects.all()}
+    """
+    Provides all categories for use in navigation menus.
+    """
+    return {"menu_categories": Category.objects.all()}
+
 
 def cart_processor(request):
-    cart = request.session.get('cart', {})
+    """
+    Provides the total number of items in the cart.
+    """
+    cart = request.session.get("cart", {})
     cart_count = sum(cart.values())
-    return {'cart_count': cart_count}
+    return {"cart_count": cart_count}
+
 
 def wishlist_processor(request):
-    wishlist = request.session.get('wishlist', [])
-    return {'wishlist_count': len(wishlist)}
+    """
+    Provides the total number of items in the wishlist.
+    """
+    wishlist = request.session.get("wishlist", [])
+    return {"wishlist_count": len(wishlist)}
+
+
+def cart_and_wishlist_count(request):
+    """
+    Combined context processor used in settings.TEMPLATES to supply:
+      - menu_categories: for the categories dropdown
+      - cart_count: total items in the cart
+      - wishlist_count: total items in the wishlist
+    """
+    cart = request.session.get("cart", {})
+    wishlist = request.session.get("wishlist", [])
+
+    return {
+        "menu_categories": Category.objects.all(),
+        "cart_count": sum(cart.values()),
+        "wishlist_count": len(wishlist),
+    }
